dfb4a367f0351fbdde443f645a6b12dd94f0bd6e
kamilrabiev2007-boop <kamil.rabiev2007@gmail.com>
2026-04-07 21:57:45 +0600

    again

--- script.js
+++ script.js
@@ -1,106 +1,50 @@
 //  1
-
-// Сохраняем строку
 localStorage.setItem("greeting", "Привет, мир!");
+console.log(localStorage.getItem("greeting"));
 
-// Получаем и выводим
-const greeting = localStorage.getItem("greeting");
-console.log("Greeting:", greeting);
+//2
+localStorage.removeItem("greeting");
+console.log(localStorage.getItem("greeting"));
 
+// 3
+let user = {
+    name: "Komil",
+    age: 19,
+    isStudent: true
+};
 
-// 2
+localStorage.setItem("user", JSON.stringify(user));
+console.log(JSON.parse(localStorage.getItem("user")));
 
-// Удаляем
-// localStorage.removeItem("greeting");
+// 4
+let updatedUser = JSON.parse(localStorage.getItem("user"));
+updatedUser.country = "Kazakhstan";
+localStorage.setItem("user", JSON.stringify(updatedUser));
+console.log(JSON.parse(localStorage.getItem("user")));
 
-// // Проверка
-// const deletedGreeting = localStorage.getItem("greeting");
-// console.log("После их удаления:", deletedGreeting); // null
+// 5
+if (localStorage.getItem("user")) {
+    console.log("Есть пользователь");
+} else {
+    localStorage.setItem("user", JSON.stringify({ name: "Test", age: 18 }));
+}
 
+// 6
+localStorage.clear();
+console.log(localStorage.getItem("user"));
 
-// // 3
+// 7
+let tasks = [
+    { title: "JS", completed: false },
+    { title: "HW", completed: false },
+    { title: "Football", completed: false }
+];
 
-// // Объект пользователя
-// const user = {
-//     name: "Komil",
-//     age: 19,
-//     isStudent: true
-// };
+localStorage.setItem("tasks", JSON.stringify(tasks));
+console.log(JSON.parse(localStorage.getItem("tasks")));
 
-// // Сохраняем как JSON
-// localStorage.setItem("user", JSON.stringify(user));
-
-// // Получаем и парсим
-// const savedUser = JSON.parse(localStorage.getItem("user"));
-// console.log("User:", savedUser);
-
-
-// // 4
-
-// // Получаем пользователя
-// let updatedUser = JSON.parse(localStorage.getItem("user"));
-
-// // Добавляем новое поле
-// updatedUser.country = "Kazakhstan";
-
-// // Сохраняем обратно
-// localStorage.setItem("user", JSON.stringify(updatedUser));
-
-// // Проверка
-// console.log("Updated User:", JSON.parse(localStorage.getItem("user")));
-
-
-// // 5
-
-// // Проверка наличия
-// if (localStorage.getItem("user")) {
-//     console.log("User exists:", JSON.parse(localStorage.getItem("user")));
-// } else {
-//     const newUser = {
-//         name: "New User",
-//         age: 20
-//     };
-//     localStorage.setItem("user", JSON.stringify(newUser));
-//     console.log("Создан новый пользователь");
-// }
-
-
-// // 6
-
-// // Полная очистка
-// localStorage.clear();
-
-// // Проверка
-// console.log("После очистки:", localStorage.getItem("user"));
-
-
-// // 7
-
-// // Массив задач
-// const tasks = [
-//     { title: "Learn JS", completed: false },
-//     { title: "Do homework", completed: false },
-//     { title: "Play football", completed: false }
-// ];
-
-// // Сохраняем
-// localStorage.setItem("tasks", JSON.stringify(tasks));
-
-// // Получаем
-// const savedTasks = JSON.parse(localStorage.getItem("tasks"));
-// console.log("Tasks:", savedTasks);
-
-
-// // 8
-
-// // Получаем задачи
-// let updatedTasks = JSON.parse(localStorage.getItem("tasks"));
-
-// // Меняем статус первой задачи
-// updatedTasks[0].completed = true;
-
-// // Сохраняем обратно
-// localStorage.setItem("tasks", JSON.stringify(updatedTasks));
-
-// // Проверка
-// console.log("Updated Tasks:", JSON.parse(localStorage.getItem("tasks")));
+// 8
+let updatedTasks = JSON.parse(localStorage.getItem("tasks"));
+updatedTasks[0].completed = true;
+localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+console.log(JSON.parse(localStorage.getItem("tasks")));
